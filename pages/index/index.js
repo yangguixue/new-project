6c821c35e9e41c732e3d53f2b9c9d85d77e2a1ce
yangguixue <1306392816@qq.com>
2017-10-25 22:05:56 +0800

    fit: 首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,17 @@ var app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {}
+    userInfo: {},
+    banners: {
+      url: [
+        '../images/banner01.jpg',
+        '../images/banner02.jpg'
+      ],
+      indicatorDots: true,
+      autoplay: true,
+      interval: 5000,
+      duration: 1000
+    }
   },
   //事件处理函数
   bindViewTap: function() {
@@ -21,6 +31,27 @@ Page({
       that.setData({
         userInfo:userInfo
       })
+    })
+  },
+  //banner
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
     })
   }
 })
